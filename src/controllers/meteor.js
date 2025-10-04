b2513b98dd8a627dfcf466759779ec03651c325a
Tu Nombre <lgarbate@gmail.com>
2025-10-04 10:16:33 -0300

    api nasa

--- src/controllers/meteor.js
+++ src/controllers/meteor.js
@@ -1,9 +1,28 @@
-import { fetchAsteroids,fetchAsteroidById } from "../services/getdata.js";
+import { fetchAsteroids, fetchAsteroidById } from "../services/getdata.js";
 
 export const getAsteroids = async (req, res) => {
   try {
     const data = await fetchAsteroids();
-    res.json(data);
+    const filteredData = {};
+    const rawdata = data.near_earth_objects;
+    for (const date in rawdata) {
+      filteredData[date] = rawdata[date].map((asteroid) => ({
+        id: asteroid.id,
+        name: asteroid.name,
+        diameter_min_m:
+          asteroid.estimated_diameter.meters.estimated_diameter_min,
+        diameter_max_m:
+          asteroid.estimated_diameter.meters.estimated_diameter_max,
+        close_approach_date:
+          asteroid.close_approach_data[0]?.close_approach_date,
+        miss_distance_km:
+          asteroid.close_approach_data[0]?.miss_distance.kilometers,
+        velocity_kph:
+          asteroid.close_approach_data[0]?.relative_velocity
+            .kilometers_per_hour,
+      }));
+    }
+    res.json(filteredData);
   } catch (error) {
     console.error("❌ Error en getAsteroids:", error.message);
     res.status(500).json({ error: "Error al obtener datos de NASA" });
@@ -12,11 +31,30 @@ export const getAsteroids = async (req, res) => {
 
 export const getAsteroidById = async (req, res) => {
   const { id } = req.params;
-    try{
-        const data = await fetchAsteroidById(id);
-        res.json(data);
-    } catch (error) {
-        console.error("❌ Error en getAsteroidById:", error.message);
-        res.status(500).json({ error: "Error al obtener datos de NASA" });
-    }
+  try {
+    const data = await fetchAsteroidById(id);
+
+    const filteredData = {};
+
+    filteredData.id = data.id;
+    filteredData.name = data.name;
+    filteredData.absolute_magnitude_h = data.absolute_magnitude_h;
+    filteredData.estimated_diameter_meters = data.estimated_diameter.meters;
+    filteredData.is_potentially_hazardous_asteroid = data.is_potentially_hazardous_asteroid;
+
+    filteredData.close_approach_data = data.close_approach_data.map((approach) => ({
+        
+        close_approach_date: approach.close_approach_date,
+        relative_velocity_kph: approach.relative_velocity.kilometers_per_hour,
+        miss_distance_km: approach.miss_distance.kilometers,
+        orbiting_body: approach.orbiting_body,
+        relative_velocity_kps: approach.relative_velocity.kilometers_per_second,
+    }));
+    filteredData.orbital_data = data.orbital_data;
+
+    res.json(filteredData);
+  } catch (error) {
+    console.error("❌ Error en getAsteroidById:", error.message);
+    res.status(500).json({ error: "Error al obtener datos de NASA" });
+  }
 };
--- src/services/getdata.js
+++ src/services/getdata.js
@@ -1,16 +1,18 @@
 import axios from "axios";
 
 export const fetchAsteroids = async () => {
-    const apiKey = process.env.NASA_API_KEY;
-    const baseUrl = process.env.NASA_BASE_URL;
-    const url = `${baseUrl}&api_key=${apiKey}`;
-    const response = await axios.get(url);
-    return response.data;
+  const apiKey = process.env.NASA_API_KEY;
+  const baseUrl = process.env.NASA_BASE_URL;
+  const url = `${baseUrl}&api_key=${apiKey}`;
+  const response = await axios.get(url);
+  return response.data;
 };
 
 export const fetchAsteroidById = async (id) => {
-    const apiKey = process.env.NASA_API_KEY;
-    const url = `https://api.nasa.gov/neo/rest/v1/neo/${id}?api_key=${apiKey}`;
-    const response = await axios.get(url);
-    return response.data;
+  const apiKey = process.env.NASA_API_KEY;
+  const url = `https://api.nasa.gov/neo/rest/v1/neo/${id}?api_key=${apiKey}`;
+  const response = await axios.get(url);
+  return response.data;
 };
+
+
