12cc4f5458a96b4dcbf7d47ed02288169fafd809
Tu Nombre <lgarbate@gmail.com>
2025-10-04 09:40:03 -0300

    api nasa

--- src/services/getdata.js
+++ src/services/getdata.js
@@ -1,3 +1,9 @@
+import axios from "axios";
+
 export const fetchAsteroids = async () => {
-  return { message: "Función fetchAsteroids no implementada" };
+    const apiKey = process.env.NASA_API_KEY;
+    const baseUrl = process.env.NASA_BASE_URL;
+    const url = `${baseUrl}&api_key=${apiKey}`;
+    const response = await axios.get(url);
+    return response.data;
 };
